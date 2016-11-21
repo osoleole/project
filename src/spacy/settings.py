a7c9ae0eb59e571482f6ebadedca84973c6d2040
osoleole <osoleole@gmail.com>
2016-11-22 01:02:52 +0900

    changed dir struct

--- src/spacy/settings.py
+++ src/spacy/settings.py
@@ -5,6 +5,8 @@ HOME = os.getenv("HOME")
 DB_ROOT = 'db'
 DATA_DIR = 'lamedoc/raw/books/txt'
 DATA_PATH = os.path.join(HOME,DB_ROOT,DATA_DIR)
+NGRAMS = 'ngrams'
+NORM = 'normalized'
 INPUT_FILE_NAME = 'clean_book1.txt'
 INPUT_FILE_PATH = os.path.join(DATA_PATH,INPUT_FILE_NAME)
 
