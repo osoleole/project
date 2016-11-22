87df7728bd2ff207ae4c84983eca7ae66bf132fd
osoleole <osoleole@gmail.com>
2016-11-22 17:16:22 +0900

    spacy's new file

--- src/spacy/settings.py
+++ src/spacy/settings.py
@@ -1,13 +1,23 @@
 import os
 
-
 HOME = os.getenv("HOME")
-DB_ROOT = 'db'
-DATA_DIR = 'lamedoc/raw/books/txt'
-DATA_PATH = os.path.join(HOME,DB_ROOT,DATA_DIR)
-NGRAMS = 'ngrams'
-NORM = 'normalized'
-INPUT_FILE_NAME = 'clean_book1.txt'
-INPUT_FILE_PATH = os.path.join(DATA_PATH,INPUT_FILE_NAME)
+DB_ROOT = os.path.join(HOME,'db')
+
+# Raw data path
+RAW_DATA_DIR = 'lamedoc/raw/sec'
+RAW_DATA_PATH = os.path.join(HOME,DB_ROOT,RAW_DATA_DIR)
+
+# Processwd data path
+PROCESSED_DATA_DIR = 'lamedoc/processed/sec'
+PROCESSED_DATA_PATH = os.path.join(HOME,DB_ROOT,PROCESSED_DATA_DIR)
+
+# N-grams data path
+NGRAMS_DATA_PATH = os.path.join(PROCESSED_DATA_PATH,'ngrams')
+
+# Normailezed data path
+NORMALIZED_DATA_PATH = os.path.join(PROCESSED_DATA_PATH,'normalized')
+
+#INPUT_FILE_NAME = 'clean_book1.txt'
+#INPUT_FILE_PATH = os.path.join(DATA_PATH,INPUT_FILE_NAME)
 
 
